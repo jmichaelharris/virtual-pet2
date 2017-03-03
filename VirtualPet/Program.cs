24fbf327bdadb1751c239ea496fcf3957b7c23df
Michael Harris <jmichaeliii@yahoo.com>
2017-03-02 19:38:57 -0500

    first commit

---
+++ VirtualPet/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string food = string.Empty;
+            string water = string.Empty;
+            string play = string.Empty;
+            VirtualPet Godzooky = new VirtualPet();
+
+            Random rnd = new Random();
+            int hungerLevel = rnd.Next(1, 17);
+            int thirstLevel = rnd.Next(1, 18);
+            int boredomLevel = rnd.Next(1, 18);
+
+            Console.WriteLine("Welcome to Michael's VirtualPet Project");
+            Console.WriteLine(" Hi, I'm Godzilla's kid, {0} ", Godzooky.name, "lets have fun");
+
+            int n = 10;
+            int results = 1;
+            while (true)
+            {
+                results *= n;
+                n--;
+          
+                {
+                int result = Environment.TickCount & Int32.MaxValue;
+                Console.WriteLine("TickCount: {0}", result);
+                }
+
+                
+                Console.WriteLine(" choose 0 Pet Option, it will pause the Virtual Pet ");
+                Console.WriteLine(" choose 1 Pet Option, it will feed {0} ", Godzooky.Name);
+                Console.WriteLine(" choose 2 Pet Option, it will give {0} some water", Godzooky.Name);
+                Console.WriteLine(" choose 3 Pet Option,{0} will talk to you", Godzooky.Name);
+                Console.WriteLine(" choose 4 Pet Option, you can play with {0} ", Godzooky.Name);
+                Console.WriteLine(" choose 5 Pet Option, you can quit the program " );
+                Console.WriteLine(" TYPE your Pet Option as a number 0, 1, 2, 3, 4 or, 5 ");
+                int petOptions = int.Parse(Console.ReadLine());
+   
+
+                switch (petOptions)
+                {
+                case 0:
+                    Console.WriteLine("OK, I'm gonna take a nap ");
+                    Thread.Sleep(0);
+                    break;
+
+                case 1:
+                     Console.WriteLine("You fed {0} ", Godzooky.Name);
+                     Godzooky.Eat(food);
+                    break;
+
+                case 2:
+                     Console.WriteLine("{0} drank some water ", Godzooky.Name);
+                     Godzooky.Drink(water);
+                    break;
+
+                case 3:
+                     Console.WriteLine(" {0}spoke ", Godzooky.Name);
+                     Godzooky.Speak();
+                    break;
+
+                case 4:
+                     Console.WriteLine(" You played with {0} ", Godzooky.Name);
+                     Godzooky.Boredom(play);
+                    break;
+                case 5:
+                     Console.WriteLine(" You're gonna miss out on a lot of fun, goodbye ");
+                     Environment.Exit(0);
+                    break;
+
+                default:
+
+                    Console.WriteLine(" Invalid selection ");
+                    Console.WriteLine(" Please choose Pet Option 0, 1, 2, 3, 4, or, 5 ");
+                    break;
+                    
+               }
+       
+           }
+        }
+    }
+}
---
+++ VirtualPet/VirtualPet.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace VirtualPet
+{
+   public class VirtualPet
+    {
+        public string name;
+        public string eat;
+        public string drink;
+        public string play;
+        private int hungerLevel;
+        private int thirstLevel;
+        private int boredomLevel;
+
+
+     public static int TickCount
+       { get; }
+
+
+
+        public VirtualPet()
+        {
+            this.name = "Godzooky";
+            this.hungerLevel = 15;
+            this.thirstLevel = 10;
+            this.boredomLevel = 5;      
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            
+        }
+        public int HungerLevel
+        {
+            get { return this.hungerLevel; }
+            set { this.hungerLevel = value; }
+        }
+        public int ThirstLevel
+        {
+            get { return this.thirstLevel; }
+            set { this.thirstLevel = value; }
+        }
+        public int BoredomLevel
+        {
+            get { return this.boredomLevel; }
+            set { this.boredomLevel = value; }
+        }
+         
+        public void Eat (string food)
+        {
+          
+             food += 2;
+        }
+
+        public void Drink (string water)
+        {
+            water += 2;
+        }
+
+        public void Boredom (string play)
+        {
+             play += 2;
+        }
+
+
+        public void Speak( )
+        {
+            if (hungerLevel == 15 && hungerLevel <= 20)
+            {
+                Console.WriteLine(" {0} dances with joy, I'm stuffed that was good food ", name);
+            }
+            else if (hungerLevel == 10 && hungerLevel <= 14)
+            {
+                Console.WriteLine(" {0} looks around with a sheepish grin, what can we do now ? ", name);
+            }
+            else if (hungerLevel == 5 && hungerLevel <= 9)
+            {
+                Console.WriteLine(" {0} looks sad, hey I'm getting hungry ", name);
+            }
+            else
+            {
+                Console.WriteLine(" {0} stomach grumbles loud, momma, momma", name);
+            }
+
+            if (thirstLevel == 15 && thirstLevel <= 20)
+            {
+                Console.WriteLine("/(h)wō/ {0} whoa that hit the hot spot ", name);
+            }
+            else if (thirstLevel == 10 && thirstLevel <= 14)
+            {
+                Console.WriteLine(" {0} looks directly at you, what can we do for fun ?", name);
+            }
+            else if (thirstLevel == 5 && thirstLevel <= 9)
+            {
+                Console.WriteLine(" {0} looks at you with dry lips, hey I'm parched", name);
+            }
+            else
+            {
+                Console.WriteLine(name, " {0} loud grumbling , momma, momma, they're a terrible pet owner ", name);
+            }
+
+            if (boredomLevel == 15 && boredomLevel <= 20)
+            {
+                Console.WriteLine(" OMG where is {0} at, ", name);
+                Console.WriteLine(" you see people running past you terrified & the building on fire  ");
+                Console.WriteLine(" ENTER PLAY, #4 QUICK, before he burns up your neighborhood");
+            }
+            else if (boredomLevel == 10 && boredomLevel <= 14)
+            {
+                Console.WriteLine(" {0} looks down at the ground, you look up, ", name);
+                Console.WriteLine( " and see the neighbors trash can on fire ");
+            }
+            else if (boredomLevel == 5 && boredomLevel <= 9)
+            {
+                Console.WriteLine(" {0} come on, lets go on a run", name);
+            }
+            else
+            {
+                Console.WriteLine(" {0} looks at you & says, lets have some fun ", name);
+            }
+        }
+        
+    }
+    
+}
